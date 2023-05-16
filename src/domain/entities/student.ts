d22844361e27a216ea0efe86772d4de7e675a42f
Hernandez <hernandez.vedovatto@gmail.com>
2023-05-15 21:00:49 -0300

    Initial Commit

---
+++ src/domain/entities/student.ts
@@ -0,0 +1,18 @@
+import { randomUUID } from "node:crypto"
+
+class Student {
+  public id: string
+  public name: string
+
+  
+  
+
+
+  constructor(name: string, id?: string){
+    this.id = id ?? randomUUID()
+    this.name = name
+  
+
+  }
+  
+}
